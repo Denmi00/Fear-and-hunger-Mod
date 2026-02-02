5d3c87e561818a299c63e528e76b4d1360e4fb6e
Den <denzen.yumi@gmail.com>
2026-02-02 16:26:39 +0100

    Add files via upload

--- Den_SaveMod.js
+++ Den_SaveMod.js
@@ -1,22 +1,28 @@
 /*:
- * @plugindesc Open Save menu trought "o" key.
+ * @plugindesc SaveManager : O to save, P pto Load.
  * @author Den
- * fell free to use it - discord : den._.mi00
  */
 
 (function() {
     document.addEventListener('keydown', function(event) {
-        
-        // This line is here to design the key "O" = "79"
-        if (event.keyCode === 79) {
+        if (SceneManager._scene instanceof Scene_Map && !SceneManager.isSceneChanging()) {
             
-            if (SceneManager._scene instanceof Scene_Map && !SceneManager.isSceneChanging()) {
-                
-                // Order the game to open saving system
+            // --- Save (O hotkey / Code 79) ---
+            if (event.keyCode === 79) {
+                // Force the saving system.
                 $gameSystem._saveEnabled = true;
-                
-                // Open the saving system
                 SceneManager.push(Scene_Save);
+            }
+
+            // --- Load (P hotkey / Code 80) ---
+            else if (event.keyCode === 80) {
+                SoundManager.playOk(); // Feedback sonore
+                try {
+                    SceneManager.push(Scene_Load);
+                } catch (e) {
+                    console.error("Erreur Loading: " + e);
+                    SoundManager.playBuzzer();
+                }
             }
         }
     });
